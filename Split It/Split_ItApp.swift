35368379d5a5a8f0e519672ff5551d4eb251ddf8
TeresaVu <teresavu353@gmail.com>
2025-02-06 17:24:06 -0500

    set up

---
+++ Split It/Split_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_ItApp.swift
+//  Split It
+//
+//  Created by Michelle Vu on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Split_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
