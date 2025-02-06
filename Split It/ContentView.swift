fa66eacca7b573f22345eeeaa15da1c7c5297f65
TeresaVu <teresavu353@gmail.com>
2025-02-06 17:24:06 -0500

    set up

---
+++ Split It/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Split It
+//
+//  Created by Michelle Vu on 2/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+   
+        Image("penguin")
+            .resizable().cornerRadius(10)
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
