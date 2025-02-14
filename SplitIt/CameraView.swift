7fc15fc9bcfe352d6af78614727972ffff62e767
Marielle <marielledoenges@ufl.edu>
2025-02-14 15:48:50 -0500

    adding basic homescreen

---
+++ SplitIt/CameraView.swift
@@ -0,0 +1,42 @@
+//
+//  CameraView.swift
+//  SplitIt
+//
+//  Created by Ian Truelsegaard on 2/14/25.
+//
+
+import SwiftUI
+import UIKit
+
+struct CameraView: UIViewControllerRepresentable {
+    @Binding var image: UIImage?       // Store the captured image
+    @Environment(\.presentationMode) var presentationMode // Dismiss the camera
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: CameraView
+
+        init(parent: CameraView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let selectedImage = info[.originalImage] as? UIImage {
+                parent.image = selectedImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+}
--- SplitIt/ContentView.swift
+++ SplitIt/ContentView.swift
@@ -1,19 +1,97 @@
-//
-//  ContentView.swift
-//  SplitIt
-//
-//  Created by Ian Truelsegaard on 2/6/25.
-//
+////
+////  ContentView.swift
+////  SplitIt
+////
+////  Created by Ian Truelsegaard on 2/6/25.
+////
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showCamera = false
+    @State private var capturedImage: UIImage?
     var body: some View {
-        Image("coffeeBackground")
-
+        ZStack{
+            Image("coffeeBackground")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            VStack{
+                Text("We hope you enjoyed\n your meal.\n\nNow let's Split-It.")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                HStack{
+                    Text("Take photo of receipt")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                    
+                    Spacer().frame(width: 20)
+                    
+                    Button(action: {
+                        showCamera = true
+                    }) {
+                        Image(systemName: "camera.fill") // SF Symbol for a camera
+                            .font(.system(size: 22))     // Adjust the icon size
+                            .foregroundColor(.white)     // Set the color
+                            .background(Circle()
+                                .fill(Color.gray)
+                                .frame(width: 40, height: 40))
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding(.top, 100)
+        }
+        .sheet(isPresented: $showCamera) {  // Presents the camera when true
+            CameraView(image: $capturedImage)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+//
+//                if let image = capturedImage {
+//                    Image(uiImage: image)
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(height: 300)
+//                        .cornerRadius(10)
+//                        .padding()
+//                } else {
+//                    Button(action: {
+//                        showCamera = true
+//                    }) {
+//                        Text("Take a Photo of Your Receipt")
+//                            .font(.headline)
+//                            .foregroundColor(.white)
+//                            .padding()
+//                            .background(Color.blue)
+//                            .cornerRadius(10)
+//                    }
+//                    .padding(.top, 20)
+//                }
+//
+//                Spacer()
+//            }
+//        }
+//        .sheet(isPresented: $showCamera) {
+//            CameraView(image: $capturedImage)
+//        }
+//    }
+//}
+//
+//#Preview {
+//    ContentView()
+//}
